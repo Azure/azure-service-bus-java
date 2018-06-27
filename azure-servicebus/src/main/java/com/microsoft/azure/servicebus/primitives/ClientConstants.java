27490ef5c525854600757a858f6b779179b2d4de
Vijaya Gopal Yarramneni <viyarr@microsoft.com>
2018-06-26 17:14:20 -0700

    Fix to get max message size from the service.

--- azure-servicebus/src/main/java/com/microsoft/azure/servicebus/primitives/ClientConstants.java
+++ azure-servicebus/src/main/java/com/microsoft/azure/servicebus/primitives/ClientConstants.java
@@ -71,7 +71,7 @@ public final class ClientConstants
 
 	public static final int MAX_MESSAGE_LENGTH_BYTES = 1024 * 1024;
 	public static final int MAX_FRAME_SIZE_BYTES = 64 * 1024;
-	public static final int MAX_EVENTHUB_AMQP_HEADER_SIZE_BYTES = 512;
+	public static final int MAX_MESSAGING_AMQP_HEADER_SIZE_BYTES = 512;
 
 	public final static Duration TIMER_TOLERANCE = Duration.ofSeconds(1);
 
